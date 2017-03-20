16c125f2673531074a8fbc26ec6cb18a4e59c20d
Zach <zachary.gordon@gmail.com>
2017-03-20 12:38:57 -0500

    added default item_types and expanded size for type string
    this is from the list that was given by CS department

--- db/schema.sql
+++ db/schema.sql
@@ -19,7 +19,7 @@ USE `mydb` ;
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `mydb`.`Item_Types` (
   `type_id` INT NOT NULL AUTO_INCREMENT,
-  `type` VARCHAR(20) NOT NULL,
+  `type` VARCHAR(50) NOT NULL,
   PRIMARY KEY (`type_id`))
 ENGINE = InnoDB;
 
@@ -53,3 +53,21 @@ ENGINE = InnoDB;
 SET SQL_MODE=@OLD_SQL_MODE;
 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
+
+
+insert into item_types (type) values('Keys');
+insert into item_types (type) values('Wallet');
+insert into item_types (type) values('Cell Phone');
+insert into item_types (type) values('Laptop/Tablet');
+insert into item_types (type) values('AU ID - Official Gov ID Card');
+insert into item_types (type) values('Flash Drive');
+insert into item_types (type) values('Textbook');
+insert into item_types (type) values('Clothing');
+insert into item_types (type) values('Bags - Purses/Backpack');
+insert into item_types (type) values('Debit/Credit Card');
+insert into item_types (type) values('Glasses');
+insert into item_types (type) values('Jewelry');
+insert into item_types (type) values('Charger');
+insert into item_types (type) values('Headphones');
+insert into item_types (type) values('Notebook/Binder');
+insert into item_types (type) values('Other');
