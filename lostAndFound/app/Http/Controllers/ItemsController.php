94aadf46fc3c0f129c6a896ce51cc81aea01471a
kschick02 <kschick02@aurora.edu>
2017-04-17 19:40:29 -0500

    Add DB class and controller

---
+++ lostAndFound/app/Http/Controllers/ItemsController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Http\Requests;
+use View;
+
+class ItemsController extends Controller {
+  public function __construct(  ) {
+    include( app_path() . '/dbClass.php');
+
+
+     public function getSearchResults(  ) {
+      $DB = new \dbClass();
+    }
+
+     public function addItem() {
+       $DB = new \dbClass();
+       $DB->addRow();
+     }
+
+     public function editItem() {
+       $DB = new \dbClass();
+       $DB->editItem();
+     }
+
+     public function dontRemember() {
+       
+     }
