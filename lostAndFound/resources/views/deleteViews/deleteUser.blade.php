0369ec44ec6b434b990e33d083a62f8d8d6b0e2d
Zach <zachary.gordon@gmail.com>
2017-04-19 00:45:37 -0500

    disallow deletion of own account in a better way

--- lostAndFound/resources/views/deleteViews/deleteUser.blade.php
+++ lostAndFound/resources/views/deleteViews/deleteUser.blade.php
@@ -8,12 +8,12 @@
             print "<h2>Current User Accounts</h2>";
             print "<table><tr><th>Name</th><th>Email</th><th>Created At</th><th>Delete</th></tr>";
             foreach($users as $val){
+                print "<tr><td>$val[name]</td><td>$val[email]</td><td>$val[created_at]</td>
+                <td>";
                 if($val['id']!=Auth::id()){
-                    print "<tr><td>$val[name]</td><td>$val[email]</td><td>$val[created_at]</td>
-                    <td>";
                     echo Form::radio('userObj', $val, array('checked' => 'checked'));
-                    print"</td></tr>";
                 }
+                print"</td></tr>";
 
             }
             print "</table>";
