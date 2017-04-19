1f3773d778c84222a61dec5d57a6f0a4ac34ab4c
Zach <zachary.gordon@gmail.com>
2017-04-19 00:33:56 -0500

    Forgot to stage this change

--- lostAndFound/app/Http/Controllers/ItemsController.php
+++ lostAndFound/app/Http/Controllers/ItemsController.php
@@ -7,24 +7,30 @@ use Illuminate\Http\Request;
 use View;
 
 class ItemsController extends Controller {
-  public function __construct(  ) {
-    include( app_path() . '/dbClass.php');
+    public function __construct(  ) {
+        //include( app_path() . '/dbClass.php');
+    }
 
 
-     public function getSearchResults(  ) {
-      $DB = new \dbClass();
+    public function getSearchResults(  ) {
+        $DB = new dbClass();
+    }
+
+    public function showForm(){
+        return view('itemViews/itemForm');
     }
 
      public function addItem() {
-       $DB = new \dbClass();
+       $DB = new dbClass();
        $DB->addRow();
      }
 
      public function editItem() {
-       $DB = new \dbClass();
+       $DB = new dbClass();
        $DB->editItem();
      }
 
      public function dontRemember() {
-       
+
      }
+ }
