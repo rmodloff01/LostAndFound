344d980dbe4e21b73d025a78911e146f9fc568ca
rmodloff01 <rmodloff01@aurora.edu>
2017-04-19 14:38:14 -0500

    Better Formatting for Show Item Form

--- lostAndFound/resources/views/home.blade.php
+++ lostAndFound/resources/views/home.blade.php
@@ -10,15 +10,31 @@
                 <div class="panel-heading">See What Items Students Have Lost!</div>
 
                 <div class="panel-body">
-                    View/Search items here, add option to delete/edit
                     <?php
-                    print "<br /> REQUEST=<pre>"; print_r( $items );
-
+                    #print "<br /> REQUEST=<pre>"; print_r( $items );
+                    print "<table>";
+                    print "<th>Item Type</th><th>Date Found</th><th>Description</th><th>Owner Info</th><th>Inventory Location</th><th>Officer</th><th>Report Number</th><th>Update Item</th>";
                     #Testing traversing of an object
                     foreach ($items as $item) {
+                      print "<tr><td>";
+                      #print "$item->type";
+                      print "</td><td>";
+                      print "$item->date_found";
+                      print "</td><td>";
                       print "$item->description";
-                      print " ";
+                      print "</td><td>";
+                      print "$item->owner_info";
+                      print "</td><td>";
+                      print "$item->inventory_location";
+                      print "</td><td>";
+                      print "$item->officer";
+                      print "</td><td>";
+                      print "$item->report_number";
+                      print "</td><td>";
+                      print "<button class='btn btn-primary'>Update</button>";
+                      print "</td></tr>";
                       }
+                    print "</div>";
                     ?>
                 </div>
 
